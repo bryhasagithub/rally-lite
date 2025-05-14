d899666ab48b18e2a6c3a9894436d09653b8ad47
Bry Nguyen <brynguyen1331@gmail.com>
2025-05-14 11:48:46 -0700

    Refactor TextAvatar to fix lint errors

--- src/components/TextAvatar.tsx
+++ src/components/TextAvatar.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
+
 type Props = {
   fullname?: string;
 };
 
-export default function TextAvatar({ fullname }: Props) {
+export const TextAvatar: React.FC<Props> = ({ fullname }) => {
   const names = fullname?.trim().split(' ');
   return (
     <span className="inline-flex items-center justify-center rounded-full bg-green-500 h-8 w-8">
       <span className="font-medium leading-none text-white">
-        {`${names[0][0]}`.toUpperCase()}
-        {names.length > 1 && `${names[names.length - 1][0]}`.toUpperCase()}
+        {names && `${names[0][0]}`.toUpperCase()}
+        {names &&
+          names.length > 1 &&
+          `${names[names.length - 1][0]}`.toUpperCase()}
       </span>
     </span>
   );
-}
+};
